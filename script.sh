9216cc6336273a9a4710fdf573f9f6f0683db5d5
Neeraj Rikhari <84703848+Nrj27@users.noreply.github.com>
2025-02-21 12:56:04 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,6 @@
+echo This is my first shell script.
+
+echo We have to mnake an script that will run an Angular program that will run on booting of system.
+
+echo It may requires additional permissions to do that.
+
